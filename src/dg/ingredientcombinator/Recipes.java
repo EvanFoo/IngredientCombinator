84f064885fd3b0a555654ee9ee3509472b221243
EvanFoo <efpepper22@gmail.com>
2014-10-30 21:20:33 -0700

    Worked on new ingredient maker.
    Need some help on the file writing

--- src/dg/ingredientcombinator/Recipes.java
+++ src/dg/ingredientcombinator/Recipes.java
@@ -1,9 +1,11 @@
 package dg.ingredientcombinator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Recipes extends Activity {
 
@@ -12,4 +14,8 @@ public class Recipes extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recipes);
 	}
+	 public void on_click_newRecipe(View view) {
+	    	Intent intent = new Intent(this, CreateNewRecipe.class);
+	    	startActivity(intent);
+	    }
 }
